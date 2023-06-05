d1d5066f999b90b6aadb7f77b5ca71dc7e32c855
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-06-05 16:33:19 +0200

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <nav>
+        <img src="/public/ygoprodeck_logo.png" alt="">
+        <span>Yu-Gi-Oh Api</span>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+@use '../style/partials/mixin.scss' as *;
+
+nav {
+    @include content-flex;
+    background-color: white;
+    padding: 20px 0;
+
+    span {
+        font-size: x-large;
+        padding-left: 10px;
+    }
+}
+</style>
